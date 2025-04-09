96746a3890e041ef95a1bdc7248690b994ceb43d
toastmanager <mrbonkyou@gmail.com>
2025-04-09 08:58:41 +0200

    style: change auth screens design according to design

--- lib/features/auth/presentation/screens/auth_screen.dart
+++ lib/features/auth/presentation/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:mpit_final_2024_app/core/components/expanded_horizontally.dart';
 import 'package:mpit_final_2024_app/core/routes/router.gr.dart';
 import 'package:mpit_final_2024_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:mpit_final_2024_app/injection.dart';
@@ -33,25 +35,36 @@ class AuthScreen extends StatelessWidget {
                         Text(
                           'Введите номер,\nчтобы продолжить',
                           textAlign: TextAlign.center,
+                          style: GoogleFonts.geologicaTextTheme().bodyMedium
+                              ?.copyWith(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w800,
+                              ),
                         ),
+                        const SizedBox(height: 80),
                         TextField(
                           controller: telController,
+                          decoration: InputDecoration(
+                            hintText: '+1 (234) 567-89-01',
+                          ),
                           inputFormatters: [telFormatter],
                         ),
                       ],
                     ),
-                    FilledButton(
-                      onPressed: () async {
-                        context.pushRoute(
-                          CodeVerifyRoute(
-                            tel: '+${telFormatter.getUnmaskedText()}',
-                          ),
-                        );
-                        await sl<AuthRepository>().requestCode(
-                          '+${telFormatter.getUnmaskedText()}',
-                        );
-                      },
-                      child: Text('Далее'),
+                    ExpandedHorizontally(
+                      child: FilledButton(
+                        onPressed: () async {
+                          context.pushRoute(
+                            CodeVerifyRoute(
+                              tel: '+${telFormatter.getUnmaskedText()}',
+                            ),
+                          );
+                          await sl<AuthRepository>().requestCode(
+                            '+${telFormatter.getUnmaskedText()}',
+                          );
+                        },
+                        child: Text('Далее'),
+                      ),
                     ),
                   ],
                 ),
